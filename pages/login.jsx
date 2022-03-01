0e30acacb5b5022da22c2f9622b13fe34f11c639
FelipeloDev <felipe.londono@pragma.com.co>
2022-02-28 22:29:21 -0500

    first commit

---
+++ pages/login.jsx
@@ -0,0 +1,36 @@
+import { getProviders, signIn } from 'next-auth/react'
+
+function Login({ providers }) {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <img
+        className="mb-5 w-52"
+        src="https://cdn.dribbble.com/users/3003809/screenshots/5992890/media/94a43a68023527759fefc27a9e84dc1e.png"
+        alt="App Logo"
+      />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="rounded-full bg-[#18D860] p-5 text-white"
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Login
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers,
+    },
+  }
+}
